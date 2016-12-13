ed90012f326888037261aab3c25bd9db446df623
eoin_a <eoin.ahern@mycit.ie>
2016-12-13 15:17:14 +0000

    added podcast model

---
+++ models/Podcast.go
@@ -0,0 +1,13 @@
+package models
+
+func GetPodcast() *Podcast {
+	var podcast = new(Podcast)
+	return podcast
+}
+
+type Podcast struct {
+	Name     string `json:"name"`
+	Category string `json:"category"`
+	WebSite  string `json:"website"`
+	Email    string `json:"email"`
+}
